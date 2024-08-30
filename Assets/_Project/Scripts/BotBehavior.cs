0b5d58eaca8bc2ea3b28f461f9b18b7a45ca3887
Nadin Chernova <nchernova2702@gmail.com>
2024-08-29 22:29:58 -0700

    refactored code

--- Assets/_Project/Scripts/BotBehavior.cs
+++ Assets/_Project/Scripts/BotBehavior.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e52e9c4037b6b3d4510f6447aa8d695825c506e2820aa2644158e9081e2dc28c
-size 3440
+oid sha256:0c9c3b757ba58a8a8d23ac3a33ffea173318519b47980e727217be4e14c9b288
+size 3438
--- Assets/_Project/Scripts/EnemyAnimator.cs
+++ Assets/_Project/Scripts/EnemyAnimator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddf82ec12de4c915b9f6ff3588fca5cae5d75726e4cec61658636e46abef1220
-size 2572
+oid sha256:328201f38ddc94623a8c660f916a8d00bdaea13041e5c0a1e14570718dc9ee5f
+size 2571
