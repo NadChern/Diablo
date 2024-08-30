ce5bce0724a408adbbdb90a1f1f518847f09a9fd
Nadin Chernova <nchernova2702@gmail.com>
2024-08-29 22:30:20 -0700

    refactored code

--- Assets/_Project/Scripts/EnemyAnimator.cs
+++ Assets/_Project/Scripts/EnemyAnimator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:328201f38ddc94623a8c660f916a8d00bdaea13041e5c0a1e14570718dc9ee5f
-size 2571
+oid sha256:c0a7e32184b8fb558f5650777e431f8243954e697865bb25a2b62209d5ef5e09
+size 2500
